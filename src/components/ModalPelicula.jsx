aaa2b35eb057ce64226c5458ca225a6d2d4c65b8
exe-quiel-dev <exe.quiel.developer@outlook.com>
2024-01-27 08:03:43 -0300

    Correccion modal

--- src/components/ModalPelicula.jsx
+++ src/components/ModalPelicula.jsx
@@ -17,7 +17,7 @@ const ModalPelicula = () => {
       </div>
       <div className="h-full w-full flex flex-col items-center justify-center">
         <h2 className="text-center uppercase font-bold text-4xl text-gray-700">{movie.title}</h2>
-        <div className="w-1/3">
+        <div className="w-1/2">
           <img src={`https://image.tmdb.org/t/p/w500/${movie.poster_path}`} alt={`Poster pelicula ${movie.title}`} className="py-6" />
         </div>
         <p className="text-center text-gray-700 py-2 text-lg">{movie.overview}</p>
